85fc44d720227e1609b811a0a92788e71ba2ea73
Tomasz Kowalski <tomasz.kowalski87@yahoo.com>
2020-11-06 20:56:45 +0100

    add employeeController, add exception class

--- src/main/java/com/rest/project/controller/EmployeeController.java
+++ src/main/java/com/rest/project/controller/EmployeeController.java
@@ -1,14 +1,29 @@
 package com.rest.project.controller;
 
+import com.rest.project.model.Employee;
 import com.rest.project.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employee")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId) {
+        throws ResourceNotFoundException {
+            Employee employee = employeeRepository.findById(employeeId)
+                    .orElseThrow(() -> new ResourceNotFoundEception("Employee not found for this id :: " + employeeId));
+            return ResponseEntity.ok().body(employee);
+        }
+    }
 }
---
+++ src/main/java/com/rest/project/exception/ResourceNotFoundEception.java
@@ -0,0 +1,14 @@
+package com.rest.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundEception extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundEception(String message) {
+        super(message);
+    }
+}
